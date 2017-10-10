a44e3f9fd1c6e24772954e1663ba0c16d02c191d
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-10-10 21:30:15 +0700

    Don't use DrawerLayoutAndroid anymore

--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
 import React, { Component } from 'react';
-import Style from './styles/GlobalStyles';
+import Theme from './styles/GlobalStyles';
 
 // Pages
-import MainPage from './components/DrawerComponent';
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import SettingPage from './pages/SettingPage';
 
 import { StackNavigator } from 'react-navigation';
+import { DrawerNavigator, DrawerItems } from 'react-navigation';
+
 import {
   AppRegistry,
   Platform,
   StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Button
 } from 'react-native';
+
+var Style = Theme.Style;
+var Color = Theme.Color;
+
+const AppDrawer = DrawerNavigator({
+  Home: {screen: HomePage},
+  Setting: {screen: SettingPage}
+}, 
+{
+  drawerWidth: 250,
+  drawerPosition: 'left',
+  contentComponent: props => {
+    <ScrollView>
+    <DrawerItems {...props} />
+    </ScrollView>}
+});
 
 const CryptoApp = StackNavigator({
   Login: {
     screen: LoginPage,
   },
   Main: {
-    screen: MainPage,
+    screen: AppDrawer,
     navigationOptions: ({navigation}) => ({
       header: null,
     }),
   },
 });
 
-
-AppRegistry.registerComponent('CryptoApp', () => CryptoApp);
+var localStyles = StyleSheet.create({
+  drawerTitle: {
+    flex: 2,
+    backgroundColor: Color.lightBlue,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  drawerItemsContainer: {
+    flex: 8, 
+    backgroundColor: Color.lightWhite,
+    padding: 2}
+  });
+  
+  
+  
+  AppRegistry.registerComponent('CryptoApp', () => CryptoApp);
+  
